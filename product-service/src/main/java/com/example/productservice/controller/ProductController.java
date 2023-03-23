596e781330676261c5fe907b13ad8f6466ed4878
M-Facey <faceymaxal17@gmail.com>
2023-03-22 21:56:14 -0500

    feat: added create product endpoint

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -1,13 +1,21 @@
 package com.example.productservice.controller;
 
 import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest) {}
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
 }

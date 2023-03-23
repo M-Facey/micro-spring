557f2e0994ec1055d0c22c51bb126cc3af0cd2f2
M-Facey <faceymaxal17@gmail.com>
2023-03-22 21:48:13 -0500

    feat: added product controller

---
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.productservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {}
+}
